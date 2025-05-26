18ce61e7cafc34129c131e3300df953c5d16fb0b
Sylvain Girault <syl@djro.fr>
2025-05-26 15:28:12 +0200

    structure de base

---
+++ index.php
@@ -0,0 +1,20 @@
+<?php
+    require_once(__DIR__.'inc/const.php');
+    $title = "Home";
+    $h1 = "Page d'accueil";
+?>
+
+<!-- HEADER -->
+<?php
+    require_once(__DIR__.'inc/header.php');
+?>
+
+<!-- MAIN -->
+<?php
+    echo "<p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Et magnam, esse labore architecto libero cupiditate nesciunt consectetur ducimus. Sit nesciunt iusto dicta nobis dolorum debitis tempore veritatis quo sint molestias! Non vitae repudiandae, cumque, nam itaque aspernatur ratione nemo repellat obcaecati porro eveniet harum architecto cum natus distinctio nesciunt dicta.</p>";
+?>
+
+<!-- FOOTER -->
+<?php
+    require_once(__DIR__.'inc/footer.php');
+?>
